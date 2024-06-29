d9685bfbe6ed86c0e4b43ca4860d585c24aaef62
mwongess <amosmwongelah@gmail.com>
2024-06-29 20:00:01 +0300

    inital files

---
+++ src/main/java/com/mwongess/styleshelf/StyleshelfApplication.java
@@ -0,0 +1,13 @@
+package com.mwongess.styleshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwongess/styleshelf/StyleshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwongess.styleshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
